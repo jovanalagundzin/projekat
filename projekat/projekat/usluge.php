6421a3bdaa2e8c0795128d861676302b6bbb36bf
jovanalagundzin <123643312+jovanalagundzin@users.noreply.github.com>
2023-06-02 12:50:24 +0200

    Add files via upload
    .

---
+++ projekat/projekat/usluge.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form actons="form.php" method="post">
+<div>
+    <lable for="3D izrada">3D izrada</lable>
+    <input type="text" id="3D izrada" name="3D izrada">
+</div>
+<div>
+    <lable for="2D izrada">2D izrada</lable>
+    <input type="text" id="2D izrada" name="2D izrada">
+</div>
+<div>
+    <lable for="cena po kvadratu">cena po kvadratu</lable>
+    <input type="text" id="cena po kvadratu" name="cena po kvadratu">
+</div>  
+<button type="submit"><Submit</button>
+    
+</body>
+</html>  
